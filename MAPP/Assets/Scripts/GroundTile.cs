441d4a0c4eda0c55489bc6ddb82fa04cf5712ff2
Razerclaws <slutbacktim@gmail.com>
2021-04-29 12:44:39 +0200

    asdd

--- MAPP/Assets/Scripts/GroundTile.cs
+++ MAPP/Assets/Scripts/GroundTile.cs
@@ -5,11 +5,16 @@ using System.Collections;
 
 public class GroundTile : MonoBehaviour
 {
+    private bool a = true;
     GroundSpawner groundSpawner;
+    
+    private float timer = 0f;
+    private float timeBeforeDeletion = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnObstacle();
         SpawnObstacle();
     }
 
@@ -18,12 +23,13 @@ public class GroundTile : MonoBehaviour
         groundSpawner.SpawnTile();
         Destroy(gameObject, 2);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+        
 
     public UnityEngine.GameObject obstaclePrefab;
     public UnityEngine.GameObject obstaclePrefabtwo;
@@ -31,6 +37,7 @@ public class GroundTile : MonoBehaviour
 
     void SpawnObstacle()
     {
+        
         GameObject[] obstacles = new GameObject[4];
         obstacles.SetValue(obstaclePrefab, 1);
         obstacles.SetValue(obstaclePrefabtwo, 2);
