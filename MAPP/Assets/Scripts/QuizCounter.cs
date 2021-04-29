459faabea4b50d6d49696c0c0360b6185c9b9a15
Razerclaws <slutbacktim@gmail.com>
2021-04-29 11:32:26 +0200

    asd

---
+++ MAPP/Assets/Scripts/QuizCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizCounter : MonoBehaviour
+{
+    private float timer = 0f;
+    [SerializeField] private float timeBeforeDeletion = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") == true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBeforeDeletion)
+            {
+               collision.GetComponent<PlayerMovement>().QuizCounter();
+            }
+            
+
+        }
+    }
+}
+
