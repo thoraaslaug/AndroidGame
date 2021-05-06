70001a7cb14094402a82ae2034ae6d1a573b1d3d
Razerclaws <slutbacktim@gmail.com>
2021-05-06 16:27:24 +0200

    Update PlayerMovement.cs

--- MAPP/Assets/Scripts/PlayerMovement.cs
+++ MAPP/Assets/Scripts/PlayerMovement.cs
@@ -65,17 +65,17 @@ public class PlayerMovement : MonoBehaviour
         if (Input.touchCount> 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
             startTouchPosition = Input.GetTouch(0).position;
         }
-    
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             endTouchPosition = Input.GetTouch(0).position;
-            
-            if ((endTouchPosition.x<startTouchPosition.x) && transform.position.x> -1.75f) 
-            transform.position = new Vector2(transform.position.x - 1.75f, transform.position.y);
-        
-            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x< 1.75f) 
-            transform.position = new Vector2(transform.position.x + 1.75f, transform.position.y);
 
+            if ((endTouchPosition.x < startTouchPosition.x) && transform.position.x > -1.75f)
+                transform.position = new Vector2(transform.position.x - 1.75f, transform.position.y);
+
+            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x < 1.75f)
+                transform.position = new Vector2(transform.position.x + 1.75f, transform.position.y);
+        }
         
 
             
