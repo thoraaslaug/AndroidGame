27a3e1a053bd7312d6cec69ed32f59a2174931e5
Razerclaws <slutbacktim@gmail.com>
2021-05-07 10:58:43 +0200

    qw

---
+++ MAPP/Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SettingsMenu settingsMenu;
+    void Settings()
+    {
+        //settingsMenu.SetActive(true);
+    }
+}
