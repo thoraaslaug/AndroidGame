aa1521be7ee4d2e5854ac259d8e6d2c2c4c9bfdf
Razerclaws <slutbacktim@gmail.com>
2021-05-16 15:58:54 +0200

    Snyggare Menyer

--- MAPP/Assets/Scripts/Menu/DeathMenu.cs
+++ MAPP/Assets/Scripts/Menu/DeathMenu.cs
@@ -11,6 +11,9 @@ public class DeathMenu : MonoBehaviour
     public GameObject deathMenu;
     public GameObject player;
     public bool isPaused;
+    public GameObject questionPanel;
+    public GameObject quizCounter;
+    public GameObject pauseButton;
 
 
     public void PauseGame()
@@ -18,13 +21,18 @@ public class DeathMenu : MonoBehaviour
         deathMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
-
+        questionPanel.SetActive(false);
+        quizCounter.SetActive(false);
+        pauseButton.SetActive(false);
 
 
     }
     public void StartOver()
     {
         deathMenu.SetActive(false);
+        questionPanel.SetActive(true);
+        quizCounter.SetActive(true);
+        pauseButton.SetActive(true);
         Time.timeScale = 1f;
         isPaused = false;
         player.GetComponent<PlayerMovement>().keepRunning();
