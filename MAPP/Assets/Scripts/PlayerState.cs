43de1feed71777b0981060852d45aa81ca7fe361
Razerclaws <slutbacktim@gmail.com>
2021-05-19 12:42:00 +0200

    Damn

--- MAPP/Assets/Scripts/PlayerState.cs
+++ MAPP/Assets/Scripts/PlayerState.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerState : MonoBehaviour
 {
@@ -58,7 +60,13 @@ public class PlayerState : MonoBehaviour
     public void quizCounter()
     {
         antalRight++;
+        Score.SaveRightToMemory(antalRight);
     }
+   
+ 
+
+        
+    
     public void setCounter()
     {
         quizAntalText.GetComponent<Text>().text = "" + antalRight;
---
+++ MAPP/Assets/Scripts/ScoreBoard/CloudOnceServiceS.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using CloudOnce;
+using System.Collections;
+
+
+public class CloudOnceServices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static CloudOnceServices instance;
+    private void Awake()
+    {
+        TestSingelton();
+    }
+    private void TestSingelton()
+    {
+        if(instance != null) { Destroy(gameObject); return; }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    public void SubmitScoreToLeaderboard(int score)
+    {
+       // Leaderboards.highScore.SubmitScore(score);
+    }
+}
