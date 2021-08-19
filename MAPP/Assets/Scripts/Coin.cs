dcacf9288bbe02e11e17b0b305966577ded81c4c
JacobCuben <71643962+JacobCuben@users.noreply.github.com>
2021-08-19 16:26:37 +0200

    1
    1

--- MAPP/Assets/Scripts/Coin.cs
+++ MAPP/Assets/Scripts/Coin.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour{
-    [SerializeField] private AudioSource pickupSound;
+    [SerializeField] private AudioSource pickupSoundSource;
     [SerializeField] private ParticleSystem particles;
+    [SerializeField] private GameObject player;
+    [SerializeField] private AudioClip pickupSound;
+    void Start() {
+        pickupSoundSource.clip = pickupSound;
+    }
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player") == true) {
-            SaveCoinsToMemory(1);
-            pickupSound.Play();
+            //SaveCoinsToMemory(1);
+            if (player.GetComponent<LifeCount>().livesRemaining < 3) {
+                player.GetComponent<LifeCount>().lives[player.GetComponent<LifeCount>().livesRemaining].enabled = true;
+                player.GetComponent<LifeCount>().livesRemaining += 1;
+                print(player.GetComponent<LifeCount>().livesRemaining);
+                player.GetComponent<PlayerState>().healthPoints += 1;
+            }
+            
+            pickupSoundSource.Play();
             particles.Play();
-            gameObject.SetActive(false);
+            StartCoroutine(deleteObject());
         }
 
     }
-    public static void SaveCoinsToMemory(int amount) {
-            PlayerPrefs.SetInt("CoinAmount", PlayerPrefs.GetInt("CoinAmount") + amount);
+
+
+    IEnumerator deleteObject() {
+        yield return new WaitForSeconds(.3f);
+        gameObject.SetActive(false);
     }
-    
+    //public static void SaveCoinsToMemory(int amount) {
+    //      PlayerPrefs.SetInt("CoinAmount", PlayerPrefs.GetInt("CoinAmount") + amount);
+    //}
+
 }
